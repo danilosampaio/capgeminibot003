fc218e97dd4cc9237733484ee61e03519ce2b4eb
Danilo Sampaio <danilo.sampaio@gmail.com>
2019-07-13 17:22:04 -0300

    Versão documentada.

--- bot-src/messages/index.js
+++ bot-src/messages/index.js
@@ -13,11 +13,28 @@ var useEmulator = true;
 var language = "pt" // idioma default
 var userId = "Capgemini";
 var userName = "Capgemini";
-var newMessageFromLiveChat = false;
+
+/**
+ * DMLiveChatConnectionSucceed: variável de controle que é atualizada quando o 
+ * VA inicia um livechat depois de receber algumas mensagems 'não conhecidas' pelo bot.
+ * A partir desse momento, o usuário estará conversando com o operador.
+ */
 var DMLiveChatConnectionSucceed = false;
+
+/**
+ * DMLiveChatEnded: variável de controle que é atualizada quando o 
+ * VA encerra o chat com o operador. A partir desse momento o usuário volta a falar com o bot.
+ */
 var DMLiveChatEnded = false;
 
+/**
+ * lastVAMessage: variável de controle que guarda a última mensagem recebida do VA.
+ */
 var lastVAMessage = "";
+
+/**
+ * lastSentMessage: variável de controle que guarda a última mensagem enviada para VA.
+ */
 var lastSentMessage = "";
 
 const listenerTimeout = 5000;
@@ -469,20 +486,34 @@ let forceFetchpoll = true;
 
 bot.dialog('fetchPoll', [
 	(session, args, next) => {
+		/**
+		 * Essa funcão atua de forma semelhante a um 'middleware' do express, onde é feito a verificação
+		 * se já existe um 'context'. Se existir, chama o next(), que irá executar função seguinte
+		 * que de fato trocar mensagens com o VA.
+		 */
 		console.log('>>>>>>>>>>>>>>>> emulador disparou a primeira função <<<<<<<<<<<<<<<')
 		console.log('>>>>>>>>> context etapa 1: ' + session.conversationData.auth);
 		
-		//console.log('Original entrada: ' + session.message.text);
+		/**
+		 * Verifica se a conversa já foi iniciada e se já tem um context(session.conversationData.auth)
+		 * antes de executar o 'getVAContext'
+		 */
 		if (!session.conversationData.started) {
 			if (!session.conversationData.auth){
 				getVAContext(session, session.message.text, next, Util.botIds.uri1);
 			}
 		} else {
+			/**
+			 * Se já existir o context, faz um bypass para a função seguinte.
+			 */
 			next();
 		}
 	},
 
 	(session, results) => {
+		/**
+		 * Função que faz a troca de mensagens com o VA através do 'getPoll'.
+		 */
 		console.log('>>>>>>>>>>>>>>>> emulador disparou a segunda função <<<<<<<<<<<<<<<')
 		console.log('>>>>>>>>> context etapa 2: ' + session.conversationData.auth);			
 		console.log({results});
@@ -493,17 +524,34 @@ bot.dialog('fetchPoll', [
 		} else {
 			console.log('INPUT Text>>>');
 			if (session.conversationData.va_message !== lastVAMessage){
+				/**
+				 * A linha abaixo foi comentada porque estava causando a exibição duplicada da mensagen
+				 * 'desculpe, não entendi o que você disse. 'para o usuário (ou emulador)
+				 */
 				//builder.Prompts.text(session, session.conversationData.va_message);
 			}
 		}
 
-		//if (forceFetchpoll) {
-			if (session.conversationData.auth){
-				getPoll(Util.botIds.uri1, session.message.text, language, userId, userName, session.conversationData.auth, session.conversationData.lastPoll, session);
-			}	
-		//}
-		//forceFetchpoll = !forceFetchpoll;	
-	}/*,
+		/**
+		 * Se já tiver context, inicia a troca de mensagens.
+		 */
+		if (session.conversationData.auth){
+			getPoll(Util.botIds.uri1, session.message.text, language, userId, userName, session.conversationData.auth, session.conversationData.lastPoll, session);
+		}
+	}
+	/**
+	 * A função abaixo foi comentada, pois o evento onFech estava com um comportamento estranho. 
+	 * De forma intercalada, uma hora dispara a primeira e a segunda função definidas no
+	 * 'bot.dialog('fetchPoll', [...', outra hora dispara apenas a terceira função, que é esta
+	 * que está comentada. Isso estava causando o seguinte erro: apenas no segundo envio de mensagem
+	 * que é getPoll era chamado, desta forma, a aplicação sempre estava uma mensagem atrasada.
+	 * Mudamos a estratégia, ao invés desta terceira função, temos apenas duas: o middle para verificar
+	 * se ja existe o context, e a função para executar o getPoll. Desta forma, resolvemos o atraso
+	 * das mensagens. Mas ainda temos um problema que é contornável: o livechat que deveria iniciar
+	 * apenas após a terceira mensagem desconhecida pelo bot, está sendo antecipada, ou seja, 
+	 * a partir da segunda mensagem já está sendo iniciado o livechat.
+	 */	
+	/*,
 	function (session, results) {
 		console.log('>>>>>>>>>>>>>>>> emulador disparou a terceira função <<<<<<<<<<<<<<<')
 		console.log('>>>>>>>>> context etapa 3: ' + session.conversationData.auth);	
@@ -580,56 +628,102 @@ bot.dialog('fetchPoll', [
 			});* /
 		});
 
-		//if (newMessageFromLiveChat){
-			listener.fetch();
-			newMessageFromLiveChat = false;
-		//}
 		// session.replaceDialog('fetchPoll');
 	}*/
 
 ]);
 
+/**
+ * getPoll: função que realiza a troca de mensagens com o VA.
+ * 	- recebe as mensagens do usuário, e encaminha para o bot.
+ *  - recebe as respostas do bot e retorna para o usuário
+ *  - quando inciado um livechat, vai buscar as respostas no VA periodicamente (3sec)
+ *  - caso o operador encerre a conversa, direciona novamente as mensagens do usuário para o bot
+ * 
+ * Parametros mais relevantes:
+ * @param {*} userInput - esse parâmetro só vem preenhido quando o getPoll é chamado do onFetch. Quando é
+ * 							chamado pelo timer que busca mensagens do VA periodicamente durante do livechat
+ * 							esse parâmetro é null, pois o tipo da request é diferente.
+ * @param {*} lastPoll - sempre que recebe uma nova mensagem do VA, o lastPoll é atualizado com base
+ * 							no serverTime que vem no response. Isso é importante para que a proxima 
+ * 							requisição pegue as respostas seguintes.
+ * @param {*} internalPoll - parâmetro que define se a chamada veio do onFetch, ou pelas chamadas periódicas
+ */
 function getPoll(uri1, userInput, language,  userId, userName, auth, lastPoll, session, internalPoll) {
 	console.log({DMLiveChatEnded, internalPoll})
+	/**
+	 * Verifica se o livechat no terminado pelo operador, e se o getPoll foi chamado do onFetch ou internamente.
+	 * Caso o livechat tenha sido encerrado, só deve executar o getPoll se o usuário enviar uma nova 
+	 * mensagem, para enviar que o loop que faz chamadas periódicas ao VA, continue fazendo requisições
+	 * desnecessárias após o livechat tiver sido encerrado.
+	 */
 	if (DMLiveChatEnded && internalPoll) {
 		return;
 	}
 	console.log({internalPoll, userInput, lastSentMessage})
-	//if ((internalPoll === undefined && userInput !== lastSentMessage) || internalPoll){
-		console.log('>>>>>> DMLiveChatConnectionSucceed: ' + DMLiveChatConnectionSucceed);
-		console.log('>>>>>>>>> internalPoll: ' + internalPoll);
-		//var type = userInput ? (DMLiveChatConnectionSucceed ? 'Livechat' : 'Assistant') : 'Talk';
-		var type = DMLiveChatConnectionSucceed ? (internalPoll ? 'Livechat' : null) : 'Assistant';
-		console.log({RequestType: type});
-		var requestData = Util.prepareRequest(type, uri1, userInput, language,  userId, userName, auth, lastPoll);
-		session.conversationData.lastPoll = new Date().getTime();
-		console.log(type == 'Livechat' ? {requestData: requestData.json.parameters} : {requestData: requestData});
+	console.log('>>>>>> DMLiveChatConnectionSucceed: ' + DMLiveChatConnectionSucceed);
+	console.log('>>>>>>>>> internalPoll: ' + internalPoll);
+	/**
+	 * Caso o livechat tenha sido iniciado, temos dois cenários:
+	 * 	- chamado interna(internalPoll): então a request deve ter o tipo 'Livechat' para buscar respostas no VA
+	 *  - chamado do onFetch: então a request deve ter o tipo null, para usar o payload padrão do 'Util.prepareRequest'
+	 * Caso o livechat não tenha iniciado:
+	 * 	- a request deve ter o tipo 'Assistant' que é uma mensagem para o bot.
+	 */
+	var type = DMLiveChatConnectionSucceed ? (internalPoll ? 'Livechat' : null) : 'Assistant';
+	console.log({RequestType: type});
+	var requestData = Util.prepareRequest(type, uri1, userInput, language,  userId, userName, auth, lastPoll);
+	session.conversationData.lastPoll = new Date().getTime();
+	console.log(type == 'Livechat' ? {requestData: requestData.json.parameters} : {requestData: requestData});
 
-		if (DMLiveChatConnectionSucceed || (internalPoll === undefined && userInput !== lastSentMessage)){
-			request.get(requestData, callbackPoll(requestData, uri1, userInput, language,  userId, userName, auth, lastPoll, session, internalPoll));
-		}
-		//if (internalPoll === undefined){
-		//	lastSentMessage = userInput;
-		//}
-	//}
+	/**
+	 * Só executa a request se o livechat tiver iniciado, ou se é uma mensagem nova do usuário
+	 */
+	if (DMLiveChatConnectionSucceed || (internalPoll === undefined && userInput !== lastSentMessage)){
+		request.get(requestData, callbackPoll(requestData, uri1, userInput, language,  userId, userName, auth, lastPoll, session, internalPoll));
+	}
 }
 
+/**
+ * Função que trata o retorno da request ao VA. Os parâmetros do 'getPoll' foram replicados aqui
+ * para possibilitar a chamada recursiva do getPoll para buscar respostas periodicamente.
+ */
 function callbackPoll (requestData, uri1, userInput, language,  userId, userName, auth, lastPoll, session, internalPoll) {
 	return function(err, res, body) {
-		let userInputAux = userInput;
 		let lastPollAux = lastPoll;
 		try {
+			/**
+			 * Se o getPoll foi chamado pelo onFetch, atualiza a última mensagem enviada.
+			 */
 			if (internalPoll === undefined){
 				lastSentMessage = userInput;
 			}
 
+			/**
+			 * A mensagem retornada pelo VA não é um JSON fora do padrão, onde as 'keys' não estão entre aspas
+			 * duplas, e os 'values' também não estão entre aspas duplas. Por isso, é necessário o regex
+			 * abaixo para padronizar a string que representa o JSON antes de fazer o parse.
+			 */
 			let mensagem = typeof body == 'object' ? body : JSON.parse(body.replace(/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g, '$1"$3":'));			
+			
+			/**
+			 * Outra despadronização do JSON é que algumas aspas vem com escape(\\), dando erro ao
+			 * fazer o parse. O stringify é feito aqui para resolver isso.
+			 */
 			mensagem = JSON.parse(JSON.stringify(mensagem));
 
+			/**
+			 * A estrutura do JSON varia de acordo com request, por isso o tratament abaixo.
+			 */
 			const typeResponse = mensagem.typeResponse || mensagem.type;
 			const codeResponse = mensagem.code || (mensagem.values ? mensagem.values.code : '');
+			
+			/**
+			 * Quando o operador encerra o chat, as variáveis abaixo precisam ser
+			 * configuradas para que a aplicação derecione novamente o usuário para o bot.
+			 */
 			if (typeResponse == "notification" &&
-			codeResponse == 'T25PcGVyYXRvckNsb3NlRGlhbG9n') {
+				codeResponse == 'T25PcGVyYXRvckNsb3NlRGlhbG9n') {
 				console.log('>>>>>>>>>>> a conversa acaba de ser fechada pelo operador.')
 				DMLiveChatConnectionSucceed = false;
 				DMLiveChatEnded = true;
@@ -637,27 +731,33 @@ function callbackPoll (requestData, uri1, userInput, language,  userId, userName
 				session.conversationData.auth = null;
 			}
 			
-			//if (body.values.text){
-			//	newMessageFromLiveChat = true;
-			//}
-			//requestData.json.parameters.lastPoll = mensagem.values ? mensagem.values.serverTime : requestData.json.parameters.lastPoll;
+			/**
+			 * Atualiza o timestamp da request.
+			 */
 			if (requestData.json){
 				requestData.json.parameters.timestamp = new Date().getTime() - 1000
 			} else {
 				requestData.timestamp = new Date().getTime() - 1000
 			}
 			
+			/**
+			 * Atualiza o lastPoll da request, para garantir que via pegar as próximas 
+			 * respostas, caso contrário, sempre recebe a primeira resposta.
+			 */
 			if (mensagem.serverTime || (mensagem.values && mensagem.values.serverTime)) {
 				lastPollAux = mensagem.serverTime || mensagem.values.serverTime;
 			} else if (requestData.json) {
 				lastPollAux = requestData.json.parameters.lastPoll;
 			}
 			
+			/**
+			 * Depois de algumas mensagens não reconhecidas pelo bot, o VA manda o comando
+			 * abaixo para iniciar um livechat. A partir daqui as requests tem o tipo 'Livechat'.
+			 */
 			if (mensagem.typeResponse == "DMLiveChatConnectionSucceed" ||
 				mensagem.typeResponse == "NAWaitingForOperator") {
 				DMLiveChatConnectionSucceed = true;
 				DMLiveChatEnded = false;
-				//userInputAux = '';
 			}
 
 			console.log('>>>>>>> callbackPoll try')
@@ -665,11 +765,20 @@ function callbackPoll (requestData, uri1, userInput, language,  userId, userName
 			console.log({lastPollAux: lastPollAux})
 
 			try {
+				/**
+				 * A estrutura do JSON de reposta varia de acordo com a request, por isso o tratamento abaixo
+				 */
 				if (mensagem.text || (mensagem.values && mensagem.values.text)){
+					/**
+					 * Covernte o texto base63 para texto plano
+					 */
 					const texto = Buffer.from(mensagem.text || mensagem.values.text, 'base64').toString();
 					console.log({text: texto});
 					session.conversationData.va_message = capHtmlToList(session, texto);
 					console.log({va_message: session.conversationData.va_message});
+					/**
+					 * Caso seja um mensagem nova, envia para o usuário.
+					 */
 					if (session.conversationData.va_message.length > 0 &&
 						session.conversationData.va_message != lastVAMessage){
 						session.send(session.conversationData.va_message);
@@ -680,13 +789,22 @@ function callbackPoll (requestData, uri1, userInput, language,  userId, userName
 				console.log(e);
 			}
 
+			/**
+			 * So executa a request recursivamente se o livechat esteja ativo, em busca de novas respostas.
+			 */
 			if (DMLiveChatConnectionSucceed && !DMLiveChatEnded){
 				//mensagem do emulador, usar talk, senão usar chatHttp
 				setTimeout(() => getPoll(uri1, null, language,  userId, userName, auth, lastPollAux, session, true), 3000);
 			}
-		} catch (e) {			
-			console.log('>>>>>>> callbackPoll catch')			
+		} catch (e) {
+			/**
+			 * Quase todo o código do try foi replicado no catch, para que o request 
+			 * recursivo não parasse em caso de erros não tratados. Contudo, após algumas 
+			 * tratamentos, não vemos mais a necessidade de manter o código abaixo após 
+			 * realizarmos mais algums testes em ambiente de homologação.
+			 */
 			console.log(e);
+			console.log('>>>>>>> callbackPoll catch');			
 			body = JSON.parse(JSON.stringify(body));
 			console.log({body: body})
 
@@ -716,7 +834,6 @@ function callbackPoll (requestData, uri1, userInput, language,  userId, userName
 			if ((typeof body == 'string' && body.indexOf('DMLiveChatConnectionSucceed') != -1) ||
 				(typeof body == 'object' && body.typeResponse =='DMLiveChatConnectionSucceed')){
 					DMLiveChatConnectionSucceed = true;
-					//userInputAux = '';
 			}			
 
 			if (DMLiveChatConnectionSucceed && !DMLiveChatEnded){
